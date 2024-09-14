e6163214194ead55cf0a7121d7751def6b9a692f
Rinc <i@rinc.xyz>
2024-09-14 16:15:05 +0800

    Add SQLite/KV wrapper for Windows;

--- src/device/DeviceInfo-windows.cxx
+++ src/device/DeviceInfo-windows.cxx
@@ -26,19 +26,8 @@ std::string NGenXX::Device::DeviceInfo::deviceManufacturer()
 
 std::string NGenXX::Device::DeviceInfo::osVersion()
 {
-    win::OSInfo *os_info = win::OSInfo::GetInstance();
-    win::OSInfo::VersionNumber vn = os_info->version_number();
-    win::OSInfo::ServicePack sp = os_info->service_pack();
-    char osv[256];
-    if (sp.major != 0)
-    {
-        sprintf(osv, "%d.%d.%d", vn.major, vn.minor, vn.build);
-    }
-    else
-    {
-        sprintf(osv, "%d.%d.%d SP%d.%d", vn.major, vn.minor, vn.build, sp.major, sp.minor);
-    }
-    return std::string(osv);
+    //TODO
+    return "";
 }
 
 int NGenXX::Device::DeviceInfo::cpuArch()
