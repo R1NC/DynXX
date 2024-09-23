9d3c0af4e20742517a746cae7b673f641c82b7cc
Rinc <i@rinc.xyz>
2024-09-23 09:55:21 +0800

    Simplify TypeUtil;

--- src/util/TypeUtil.hxx
+++ src/util/TypeUtil.hxx
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <tuple>
+#include "../../include/NGenXXTypes.h"
 
 static inline const char *str2charp(std::string s)
 {
@@ -12,12 +13,12 @@ static inline const char *str2charp(std::string s)
     return nc;
 }
 
-static inline const unsigned char *copyBytes(std::tuple<const unsigned char *, const unsigned int> t)
+static inline const byte *copyBytes(std::tuple<const byte *, const size> t)
 {
-    const unsigned char *cs = std::get<0>(t);
+    const byte *cs = std::get<0>(t);
     int len = std::get<1>(t);
     if (cs == NULL || len <= 0) return NULL;
-    const unsigned char *ncs = (unsigned char *)malloc(len + 1);
+    const byte *ncs = (byte *)malloc(len + 1);
     memset((void *)ncs, 0, len + 1);
     memcpy((void *)ncs, cs, len);
     return ncs;
