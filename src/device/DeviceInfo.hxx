06f7099aa3f21b00d7b03b2a007b6c7b1641c3c2
Rinc <i@rinc.xyz>
2024-11-08 10:13:06 +0800

    Fix const char;

--- src/device/DeviceInfo.hxx
+++ src/device/DeviceInfo.hxx
@@ -22,30 +22,30 @@ namespace NGenXX
             int cpuArch();
 
 #ifdef __ANDROID__
-            char const *SYS_PROPERTY_VERSION_RELEASE = "ro.build.version.release";
-            char const *SYS_PROPERTY_VERSION_INCREMENTAL = "ro.build.version.incremental";
-            char const *SYS_PROPERTY_VERSION_CODENAME = "ro.build.version.codename";
-            char const *SYS_PROPERTY_VERSION_SDK = "ro.build.version.sdk";
+            char const *const SYS_PROPERTY_VERSION_RELEASE = "ro.build.version.release";
+            char const *const SYS_PROPERTY_VERSION_INCREMENTAL = "ro.build.version.incremental";
+            char const *const SYS_PROPERTY_VERSION_CODENAME = "ro.build.version.codename";
+            char const *const SYS_PROPERTY_VERSION_SDK = "ro.build.version.sdk";
 
-            char const *SYS_PROPERTY_MODEL = "ro.product.model";
-            char const *SYS_PROPERTY_MANUFACTURER = "ro.product.manufacturer";
-            char const *SYS_PROPERTY_BOARD = "ro.product.board";
-            char const *SYS_PROPERTY_BRAND = "ro.product.brand";
-            char const *SYS_PROPERTY_DEVICE = "ro.product.device";
-            char const *SYS_PROPERTY_PRODUCT = "ro.product.name";
+            char const *const SYS_PROPERTY_MODEL = "ro.product.model";
+            char const *const SYS_PROPERTY_MANUFACTURER = "ro.product.manufacturer";
+            char const *const SYS_PROPERTY_BOARD = "ro.product.board";
+            char const *const SYS_PROPERTY_BRAND = "ro.product.brand";
+            char const *const SYS_PROPERTY_DEVICE = "ro.product.device";
+            char const *const SYS_PROPERTY_PRODUCT = "ro.product.name";
 
-            char const *SYS_PROPERTY_HARDWARE = "ro.hardware";
+            char const *const SYS_PROPERTY_HARDWARE = "ro.hardware";
 
-            char const *SYS_PROPERTY_CPU_ABI = "ro.product.cpu.abi";
-            char const *SYS_PROPERTY_CPU_ABI2 = "ro.product.cpu.abi2";
+            char const *const SYS_PROPERTY_CPU_ABI = "ro.product.cpu.abi";
+            char const *const SYS_PROPERTY_CPU_ABI2 = "ro.product.cpu.abi2";
 
-            char const *SYS_PROPERTY_DISPLAY = "ro.build.display.id";
-            char const *SYS_PROPERTY_HOST = "ro.build.host";
-            char const *SYS_PROPERTY_USER = "ro.build.user";
-            char const *SYS_PROPERTY_ID = "ro.build.id";
-            char const *SYS_PROPERTY_TYPE = "ro.build.type";
-            char const *SYS_PROPERTY_TAGS = "ro.build.tags";
-            char const *SYS_PROPERTY_FINGERPRINT = "ro.build.fingerprint";
+            char const *const SYS_PROPERTY_DISPLAY = "ro.build.display.id";
+            char const *const SYS_PROPERTY_HOST = "ro.build.host";
+            char const *const SYS_PROPERTY_USER = "ro.build.user";
+            char const *const SYS_PROPERTY_ID = "ro.build.id";
+            char const *const SYS_PROPERTY_TYPE = "ro.build.type";
+            char const *const SYS_PROPERTY_TAGS = "ro.build.tags";
+            char const *const SYS_PROPERTY_FINGERPRINT = "ro.build.fingerprint";
 
             const std::string sysProperty(const std::string &k);
 
