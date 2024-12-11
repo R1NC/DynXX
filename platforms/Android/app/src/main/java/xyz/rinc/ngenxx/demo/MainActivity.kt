83af73c6fda212f1cf765914c8b5bd9697d9a286
Rinc <i@rinc.xyz>
2024-12-11 12:00:24 +0800

    Simplify Android demo;

--- platforms/Android/app/src/main/java/xyz/rinc/ngenxx/demo/MainActivity.kt
+++ platforms/Android/app/src/main/java/xyz/rinc/ngenxx/demo/MainActivity.kt
@@ -1,44 +1,25 @@
 package xyz.rinc.ngenxx.demo
 
-import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
 import xyz.rinc.ngenxx.NGenXX
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
 class MainActivity : ComponentActivity() {
 
     private val reqPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
-    ) { isGranted ->
+    ) { _ ->
         //if (isGranted) {
             NGenXXTest.init(application)
         //}
     }
 
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
         if (Build.VERSION.SDK_INT < 32 &&
             ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
@@ -47,38 +28,11 @@ class MainActivity : ComponentActivity() {
             NGenXXTest.init(application)
         }
 
-        setContent {
-            Scaffold(modifier = Modifier.fillMaxSize()) {
-                Greeting()
-            }
-        }
+        NGenXXTest.goAllAsync(100)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         NGenXX.release()
     }
-}
-
-@Composable
-fun Greeting() {
-    Column(
-        modifier = Modifier.fillMaxWidth().fillMaxHeight(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Button(
-            onClick = {
-                NGenXXTest.goAllAsync(100)
-            },
-        ) {
-            Text("test")
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Greeting()
 }
