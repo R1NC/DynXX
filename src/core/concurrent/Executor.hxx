1596b397f9188d8ba86cf55962abc18bd0898ba3
Rinc <i@rinc.xyz>
2025-06-18 00:36:51 +0800

    Update Executor.hxx

--- src/core/concurrent/Executor.hxx
+++ src/core/concurrent/Executor.hxx
@@ -38,7 +38,7 @@ namespace NGenXX::Core::Concurrent {
     private:
         size_t sleepMicroSecs{1000uz};
         std::timed_mutex mutex;
-        std::queue<std::unique_ptr<TaskT>> taskQueue;
+        std::queue<TaskT> taskQueue;
 #if defined(__cpp_lib_jthread)
         std::jthread thread;
 #else
