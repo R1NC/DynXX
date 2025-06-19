da0faf4efb1affc3bc1fb1a43214bebc50ffa7d9
Rinc <i@rinc.xyz>
2025-06-19 13:06:44 +0800

    Check availability of C++ new API for macOS.

--- include/NGenXXMacro.hxx
+++ include/NGenXXMacro.hxx
@@ -12,21 +12,21 @@
 #define EXPORT_AUTO EXPORT
 #endif
 
-#define NGENXX_IOS_VERSION_CHECK(version) (!defined(__IPHONE_OS_VERSION_MIN_REQUIRED) || (__IPHONE_OS_VERSION_MIN_REQUIRED >= version))
+#define CHECK_APPLE_VERSION(iosV, macV) (!defined(__IPHONE_OS_VERSION_MIN_REQUIRED) || (__IPHONE_OS_VERSION_MIN_REQUIRED >= iosV) && (!defined(__MAC_OS_X_VERSION_MIN_REQUIRED) || (__MAC_OS_X_VERSION_MIN_REQUIRED >= macV)))
 
 #if defined(__cpp_lib_to_chars)
-#if NGENXX_IOS_VERSION_CHECK(160500)
+#if CHECK_APPLE_VERSION(160500, 130400)
 #if (__cpp_lib_to_chars >= 201611L)
-            #define USE_STD_CHAR_CONV_INT
+#define USE_STD_CHAR_CONV_INT
 #endif
 #if (__cpp_lib_to_chars >= 201907L)
-            #define USE_STD_CHAR_CONV_FLOAT
+#define USE_STD_CHAR_CONV_FLOAT
 #endif
 #endif
 #endif
 
 #if defined(__cpp_lib_format)
-#if NGENXX_IOS_VERSION_CHECK(160500)
+#if CHECK_APPLE_VERSION(160500, 130400)
 #define USE_STD_FORMAT
 #endif
 #endif
