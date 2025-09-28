bff366c8868e2076da72098c324e6ac3bda3d67b
Rinc <i@rinc.xyz>
2025-09-28 09:11:43 +0800

    Update build-Android.sh

--- build-tools/build-Android.sh
+++ build-tools/build-Android.sh
@@ -2,7 +2,7 @@
 
 source "$(dirname "$0")/build-utils.sh"
 
-NDK_ROOT=${NDK_ROOT:-"~/Library/Android/sdk/ndk/29.0.14033849/"}
+NDK_ROOT=${NDK_ROOT:-"$HOME/Library/Android/sdk/ndk/29.0.14033849/"}
 ARCH=arm64-v8a
 VER=24
 
