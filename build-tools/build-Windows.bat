09d275701f348aa166386f89d052b7ede9c6dbd4
Rinc Liu <i@rinc.xyz>
2025-10-11 21:38:55 +0800

    determine CMake Generator by build type for Windows.

--- build-tools/build-Windows.bat
+++ build-tools/build-Windows.bat
@@ -2,9 +2,7 @@ if not defined VCPKG_ROOT (
     set "VCPKG_ROOT=C:/Users/rinc/vcpkg"
 )
 
-set "GENERATOR=Visual Studio 17 2022"
-set "VCPKG_TARGET=x64-windows"
-
+set "VCPKG_TARGET=x64-windows-static"
 set "BUILD_TYPE=Release"
 
 cd ..
@@ -21,18 +19,30 @@ md "%HEADER_OUTPUT_DIR%" 2>nul
 md "%LIB_OUTPUT_DIR%" 2>nul
 cd "%BUILD_DIR%"
 
+if /i "%BUILD_TYPE%"=="Debug" (
+    set "GENERATOR=Visual Studio 17 2022"
+    set "CMAKE_CONFIG_C_COMPILER="
+    set "CMAKE_CONFIG_CXX_COMPILER="
+    set "CMAKE_CONFIG_EXTRA=-T ClangCL"
+) else (
+    set "GENERATOR=Ninja"
+    set "CMAKE_CONFIG_C_COMPILER=-DCMAKE_C_COMPILER=clang-cl"
+    set "CMAKE_CONFIG_CXX_COMPILER=-DCMAKE_CXX_COMPILER=clang-cl"
+    set "CMAKE_CONFIG_EXTRA="
+)
+
 cmake .. ^
     -G "%GENERATOR%" ^
     -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake" ^
     -DVCPKG_TARGET_TRIPLET="%VCPKG_TARGET%" ^
-    -DCMAKE_C_COMPILER=clang-cl ^
-    -DCMAKE_CXX_COMPILER=clang-cl ^
+    %CMAKE_CONFIG_C_COMPILER% ^
+    %CMAKE_CONFIG_CXX_COMPILER% ^
     -DCMAKE_BUILD_TYPE="%BUILD_TYPE%" ^
     -DCMAKE_INSTALL_PREFIX=. ^
     -DCMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE="%LIB_OUTPUT_DIR%" ^
     -DCMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE="%LIB_OUTPUT_DIR%" ^
     -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE="%LIB_OUTPUT_DIR%" ^
-    -T ClangCL
+    %CMAKE_CONFIG_EXTRA%
 
 cmake --build . --config "%BUILD_TYPE%"
 cmake --install . --prefix "%OUTPUT_DIR%" --component headers
