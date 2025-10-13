02c23d7591fd67a59916fe5694620fc720f0f56c
Rinc Liu <i@rinc.xyz>
2025-10-13 21:51:10 +0800

    CMakePresets.json for Linux.

--- build-tools/build-Linux.sh
+++ build-tools/build-Linux.sh
@@ -3,44 +3,35 @@
 source "$(dirname "$0")/build-utils.sh"
 
 #TODO
-VCPKG_ROOT=${VCPKG_ROOT:-"$HOME/dev/vcpkg/"}
-VCPKG_TARGET=x64-linux
+export VCPKG_ROOT=${VCPKG_ROOT:-"$HOME/dev/vcpkg/"}
+export VCPKG_TARGET=x64-linux
+export BUILD_FOLDER=build.Linux
+export OUTPUT_PATH=${BUILD_FOLDER}/output
+export OUTPUT_LIB_PATH=$PWD/${OUTPUT_PATH}/libs
+export OUTPUT_EXE_PATH=$PWD/${OUTPUT_PATH}/exe
 
 DEBUG=0
-
-cd ..
-export VCPKG_BINARY_SOURCES="clear;default,readwrite"
-$VCPKG_ROOT/vcpkg install --triplet=${VCPKG_TARGET}
-
-BUILD_DIR=build.Linux
-rm -rf ${BUILD_DIR}
-mkdir -p ${BUILD_DIR}
-cd ${BUILD_DIR}
-
-LIB_TYPE="Release"
+BUILD_TYPE="release"
 if [ $DEBUG == 1 ]; then
-    LIB_TYPE="Debug"
+    BUILD_TYPE="debug"
 fi
 
-LIB_OUTPUT_DIR=$(pwd)/output/libs
-mkdir -p ${LIB_OUTPUT_DIR}
+cd ..
+rm -rf ${BUILD_FOLDER}
+mkdir -p ${OUTPUT_LIB_PATH}
 
-cmake .. \
-    -DCMAKE_INSTALL_PREFIX=. \
-    -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake \
-    -DVCPKG_TARGET_TRIPLET=$VCPKG_TARGET \
-    -DCMAKE_BUILD_TYPE=${LIB_TYPE} \
-    -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY=${LIB_OUTPUT_DIR} \
-    -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=${LIB_OUTPUT_DIR} \
-    -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=${LIB_OUTPUT_DIR}
+cmake --preset linux-${BUILD_TYPE}
 
-build_with_cmake . ${BUILD_DIR} ${LIB_TYPE}
+$VCPKG_ROOT/vcpkg install --triplet=${VCPKG_TARGET}
+
+cmake --build --preset linux-${BUILD_TYPE}
+cmake --install ${BUILD_FOLDER} --prefix ${OUTPUT_PATH} --component headers
 
 #Checking Artifacts
 ARTIFACTS=(
-    "${LIB_OUTPUT_DIR}/libDynXX.a"
-    "${LIB_OUTPUT_DIR}/libqjs.a"
-    "${LIB_OUTPUT_DIR}/libmmkvcore.a"
-    "${LIB_OUTPUT_DIR}/libmmkv.a"
+    "${OUTPUT_LIB_PATH}/libDynXX.a"
+    "${OUTPUT_LIB_PATH}/libqjs.a"
+    "${OUTPUT_LIB_PATH}/libmmkvcore.a"
+    "${OUTPUT_LIB_PATH}/libmmkv.a"
 )
 check_artifacts "${ARTIFACTS[@]}"
