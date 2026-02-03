b42512ef8c4a0c2a68bcba8bd19887dace067d8f
Rinc <i@rinc.xyz>
2026-02-03 21:14:12 +0800

    fix ps1

--- build-tools/build-Windows.ps1
+++ build-tools/build-Windows.ps1
@@ -1,9 +1,9 @@
 Set-Location ..
 
-$env:PLATFORM = "Windows"
-$env:BUILD_TYPE = "Release"
-$env:WINDOWS_ABI = "x64"
-$PRESET = "$env:PLATFORM-$env:BUILD_TYPE"
+$PLATFORM = "Windows"
+$BUILD_TYPE = "Release"
+$WINDOWS_ABI = "x64"
+$PRESET = "$PLATFORM-$BUILD_TYPE"
 
 if ($env:CI_VCPKG_ROOT) {
     $env:VCPKG_ROOT = $env:CI_VCPKG_ROOT
@@ -15,22 +15,22 @@ if ($env:CI_VCPKG_BINARY_SOURCES) {
 } else {
     $env:VCPKG_BINARY_SOURCES = "files,$env:USERPROFILE/vcpkg-binary-cache,readwrite"
 }
-$env:VCPKG_TARGET_TRIPLET = "$env:WINDOWS_ABI-windows-static"
+$env:VCPKG_TARGET_TRIPLET = "$WINDOWS_ABI-windows-static"
 
-$BUILD_FOLDER = "build.$env:PLATFORM/$env:BUILD_TYPE"
-$OUTPUT_FOLDER = "$BUILD_FOLDER/output"
-$OUTPUT_PATH = "$PWD/$OUTPUT_FOLDER/$env:WINDOWS_ABI"
-$OUTPUT_LIB_PATH = "$OUTPUT_PATH/lib"
-$OUTPUT_DLL_PATH = "$OUTPUT_PATH/share"
-$OUTPUT_EXE_PATH = "$OUTPUT_PATH/bin"
+$env:BUILD_FOLDER = "build.$PLATFORM/$BUILD_TYPE"
+$OUTPUT_FOLDER = "$env:BUILD_FOLDER/output"
+$OUTPUT_PATH = "$PWD/$OUTPUT_FOLDER/$WINDOWS_ABI"
+$env:OUTPUT_LIB_PATH = "$OUTPUT_PATH/lib"
+$env:OUTPUT_DLL_PATH = "$OUTPUT_PATH/share"
+$env:OUTPUT_EXE_PATH = "$OUTPUT_PATH/bin"
 
 & cmake --preset "$PRESET"
 & cmake --build --preset "$PRESET"
-& cmake --install "$BUILD_FOLDER" --prefix "$OUTPUT_FOLDER" --component headers
+& cmake --install "$env:BUILD_FOLDER" --prefix "$OUTPUT_FOLDER" --component headers
 
 foreach ($f in @(
-    "$OUTPUT_LIB_PATH/DynXX.lib",
-    "$OUTPUT_EXE_PATH/qjsc.exe"
+    "$env:OUTPUT_LIB_PATH/DynXX.lib",
+    "$env:OUTPUT_EXE_PATH/qjsc.exe"
 )) {
     if (-not (Test-Path $f)) {
         Write-Host "ARTIFACT NOT FOUND: $f"
