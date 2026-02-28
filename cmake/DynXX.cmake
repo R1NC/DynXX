e7858bf8305cd958c2207aa744b2990d1e892b38
Rinc <i@rinc.xyz>
2026-02-28 17:50:25 +0800

    Update DynXX.cmake

--- cmake/DynXX.cmake
+++ cmake/DynXX.cmake
@@ -156,6 +156,67 @@ function(installHeaders SOURCE_DIR DESTINATION_DIR)
     )
 endfunction()
 
+## Install Sanitizers Runtime
+function(install_asan_runtime dest_dir)
+	if(APPLE)
+		if(CMAKE_OSX_SYSROOT MATCHES "Simulator|iPhoneSimulator")
+			set(_asan_lib_name "libclang_rt.asan_iossim_dynamic.dylib")
+		elseif(CMAKE_OSX_SYSROOT MATCHES "iPhoneOS|iphoneos")
+			set(_asan_lib_name "libclang_rt.asan_ios_dynamic.dylib")
+		elseif(CMAKE_OSX_SYSROOT MATCHES "MacOSX|macosx")
+			set(_asan_lib_name "libclang_rt.asan_osx_dynamic.dylib")
+		else()
+			message(SEND_ERROR "install_asan_runtime: UNKNOWN CMAKE_OSX_SYSROOT: ${CMAKE_OSX_SYSROOT}")
+			return()
+		endif()
+	elseif(CMAKE_SYSTEM_NAME STREQUAL "Android" OR CMAKE_SYSTEM_NAME STREQUAL "OHOS")
+		set(_abi "${CMAKE_ANDROID_ARCH_ABI}")
+		if(CMAKE_SYSTEM_NAME STREQUAL "OHOS" AND NOT _abi)
+			set(_abi "${CMAKE_SYSTEM_PROCESSOR}")
+		endif()
+		if(_abi STREQUAL "arm64-v8a" OR _abi STREQUAL "aarch64")
+			set(_asan_lib_name "libclang_rt.asan-aarch64-android.so")
+		elseif(_abi STREQUAL "armeabi-v7a" OR _abi STREQUAL "armv7a" OR _abi STREQUAL "arm")
+			set(_asan_lib_name "libclang_rt.asan-arm-android.so")
+		elseif(_abi STREQUAL "x86_64")
+			set(_asan_lib_name "libclang_rt.asan-x86_64-android.so")
+		elseif(_abi STREQUAL "x86" OR _abi STREQUAL "i686")
+			set(_asan_lib_name "libclang_rt.asan-i686-android.so")
+		else()
+			message(SEND_ERROR "install_asan_runtime: UNKNOWN ABI: ${_abi}")
+			return()
+		endif()
+	else()
+		message(SEND_ERROR "install_asan_runtime: Only support APPLE / Android / OHOS (CMAKE_SYSTEM_NAME=${CMAKE_SYSTEM_NAME})")
+		return()
+	endif()
+
+	execute_process(
+		COMMAND ${CMAKE_CXX_COMPILER} -print-file-name=${_asan_lib_name}
+		OUTPUT_VARIABLE _asan_lib_full_path
+	)
+
+	string(STRIP "${_asan_lib_full_path}" _asan_lib_full_path)
+
+	if(NOT EXISTS "${_asan_lib_full_path}" AND (CMAKE_SYSTEM_NAME STREQUAL "Android" OR CMAKE_SYSTEM_NAME STREQUAL "OHOS"))
+		if(NOT _asan_lib_full_path MATCHES "^[/\\\\]|^[A-Za-z]:")
+			execute_process(
+				COMMAND ${CMAKE_CXX_COMPILER} -print-resource-dir
+				OUTPUT_VARIABLE _clang_resource_dir
+			)
+			string(STRIP "${_clang_resource_dir}" _clang_resource_dir)
+			set(_asan_lib_full_path "${_clang_resource_dir}/lib/linux/${_asan_lib_name}")
+		endif()
+	endif()
+
+	if(EXISTS "${_asan_lib_full_path}")
+		file(COPY "${_asan_lib_full_path}" DESTINATION "${dest_dir}")
+		message(STATUS "ASan runtime lib is installed: ${_asan_lib_full_path} -> ${dest_dir}/${_asan_lib_name}")
+	else()
+		message(SEND_ERROR "ASan lib not found: ${_asan_lib_name} (path: ${_asan_lib_full_path})")
+	endif()
+endfunction()
+
 ## Print Common Build Information
 function(printBaseBuildInfo)
     message(" └─ CCACHE: ${CCACHE_FOUND}")
