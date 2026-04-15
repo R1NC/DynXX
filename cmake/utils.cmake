ec54a2faff8c23aa7096e79b39dfbb33ef7a315a
Rinc <i@rinc.xyz>
2026-04-15 08:33:24 +0800

    optmz

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,4 +1,7 @@
 function(init_before_project)
+    # Policy CMP0091: Controls MSVC runtime library flags.
+    # NEW behavior: Use CMAKE_MSVC_RUNTIME_LIBRARY to set /MD, /MT, etc.
+    # Available since CMake 3.15
     if(POLICY CMP0091)
         cmake_policy(SET CMP0091 NEW)
     endif()
@@ -44,21 +47,29 @@ function(check_apple_version_limit iosV macV RESULT_VAR)
     endif()
 endfunction()
 
+# Args:
+#   TARGET_NAME - Required, target name for include directories
+#   lib         - Required, library name for FetchContent
+#   url         - Required, git repository URL
+#   tag         - Required, git tag/branch or commit hash
+#   INC_DIR     - Optional, include directory path relative to repo root
+#   SRC_DIR     - Optional, source directory path relative to repo root
+#   MANUAL_ADD  - Optional, BOOL, call add_subdirectory manually, default OFF
 function(add_git_lib TARGET_NAME lib url tag)
     if(NOT TARGET_NAME OR NOT lib OR NOT url OR NOT tag)
         message(FATAL_ERROR "`add_git_lib()`: `TARGET_NAME`, `lib`, `url`, `tag` are required!")
     endif()
-    set(inc_dir ${ARGV4})
-    set(src_dir ${ARGV5})
-    set(manual_add ${ARGV6})
-    if(NOT src_dir)
-        set(src_dir "")
+
+    cmake_parse_arguments(ARG "" "INC_DIR;SRC_DIR;MANUAL_ADD" "" ${ARGN})
+
+    if(NOT ARG_INC_DIR)
+        set(ARG_INC_DIR "")
     endif()
-    if(NOT inc_dir)
-        set(inc_dir "")
+    if(NOT ARG_SRC_DIR)
+        set(ARG_SRC_DIR "")
     endif()
-    if(NOT DEFINED manual_add)
-        set(manual_add OFF)
+    if(NOT DEFINED ARG_MANUAL_ADD)
+        set(ARG_MANUAL_ADD OFF)
     endif()
 
     FetchContent_Declare(
@@ -71,10 +82,10 @@ function(add_git_lib TARGET_NAME lib url tag)
     )
     FetchContent_MakeAvailable(${lib})
 
-    set(inc_path ${${lib}_SOURCE_DIR}/${inc_dir})
-    set(src_path ${${lib}_SOURCE_DIR}/${src_dir})
+    set(inc_path ${${lib}_SOURCE_DIR}/${ARG_INC_DIR})
+    set(src_path ${${lib}_SOURCE_DIR}/${ARG_SRC_DIR})
 
-    if(manual_add)
+    if(ARG_MANUAL_ADD)
         if(TARGET ${lib})
             message(WARNING "${lib} already exists, skip `add_subdirectory()`")
         else()
